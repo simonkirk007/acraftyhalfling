fedb1b38edccfe00775606bb6bb2b37cbbf7c6d7
Simon Kirk <124763876+simonkirk007@users.noreply.github.com>
2023-03-13 10:17:34 +0000

    1

--- assets/app.js
+++ assets/app.js
@@ -2,11 +2,11 @@ jQuery(document).ready(function(){
     jQuery('.related-products ul.grid.product-grid').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
-        arrows: ture,
+        arrows: ture
       });
     jQuery('.prod-slider').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
-        arrows: false,
+        arrows: false
       });
   });
