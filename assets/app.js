b0d18e2bea9fa620ed8b3a7778689fa9713e0cd9
Simon Kirk <124763876+simonkirk007@users.noreply.github.com>
2023-03-26 15:56:26 +0000

    1

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function(){
-    jQuery('.related-products ul.grid.product-grid').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows: ture
-      });
+    // jQuery('.related-products ul.grid.product-grid').slick({
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     arrows: ture
+    //   });
     jQuery('.prod-slider').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
